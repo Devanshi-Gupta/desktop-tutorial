d3a905ea5d5dc3b659ab18b8de3d590c1dc4412e
Devanshi-Gupta <devanshiknp15@gmail.com>
2022-07-02 23:28:50 +0530

    4. update and change password API

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,11 @@ const connection = require('../connection');
 const router = express.Router();
 
 const jwt = require('jsonwebtoken');
-const nodemailer =require('nodemailer');
+const nodemailer = require('nodemailer');
 
 require('dotenv').config();
-
+var auth = require('../services/authentication');
+var checkRole = require('../services/checkRole');
 router.post('/signup', (req, res) => {
     let user = req.body;
     var query = "select email,password,role,status from user where email=?"
@@ -56,41 +57,98 @@ router.post('/login', (req, res) => {
     });
 })
 
-var transporter =nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASSWORD
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 })
 
-router.post('/forgotPassword',(req,res)=>{
-    const user=req.body;
-    var query="select email,password from user where email=?";
-    connection.query(query,[user.email],(err,results)=>{
-        if(!err){
-            if(results.length<=0){
-                return res.status(200).json({message:"Password sent successfully to your email."});
-            }else{
-                var mailOptions={
-                    to:process.env.EMAIL,
-                    from:results[0].email,
-                    subject:'Password by Cafe Management System',
-                    html:'<p><b>Your Login details for Cafe Management System</b><br><b>Email:</b>'+results[0].email+'<br><b>Password: </b>'+results[0].password+'<br><a href="http://localhost:4200/user/login">Click here to login</a></p>'
+router.post('/forgotPassword', (req, res) => {
+    const user = req.body;
+    var query = "select email,password from user where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(200).json({ message: "Password sent successfully to your email." });
+            } else {
+                var mailOptions = {
+                    to: process.env.EMAIL,
+                    from: results[0].email,
+                    subject: 'Password by Cafe Management System',
+                    html: '<p><b>Your Login details for Cafe Management System</b><br><b>Email:</b>' + results[0].email + '<br><b>Password: </b>' + results[0].password + '<br><a href="http://localhost:4200/user/login">Click here to login</a></p>'
                 };
-                transporter.sendMail(mailOptions,function(error,info){
-                    if(error){
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
                         console.log(error);
-                    }else{
-                        console.log('Email sent: '+info.response)
+                    } else {
+                        console.log('Email sent: ' + info.response)
                     }
                 });
-                return res.status(200).json({message:"Password sent successfully to your email."});
+                return res.status(200).json({ message: "Password sent successfully to your email." });
             }
-        }else{
+        } else {
             return res.status(500).json(err);
         }
     });
+})
+
+router.get('/get', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    var query = "select id,name,email,contactNumber,status from user where role='user'";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    let user = req.body;
+    var query = "update user set status=? where id=?";
+    connection.query(query,[user.status,user.id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "User id does not exist" });
+            }
+            return res.status(200).json({ message: "User Updated Successfully" })
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.get('/checkToken', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    return res.status(200).json({ message: "true" });
+})
+
+router.post('/changePassword',auth.authenticateToken, (req, res) => {
+    const user = req.body;
+    const email = res.locals.email;
+    var query = "select * from user where email=? and password=?";
+    connection.query(query, [email, user.oldPassword], (err, results) => {
+        if (!err) {
+            if (results.length <= 0)
+                return res.status(400).json({ message: "Incorrect old password" });
+            else if (results[0].password == user.oldPassword) {
+                query = "update user set password=? where email=?";
+                connection.query(query, [user.newPassword, email], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Password Updated Successfully." })
+                    } else {
+                        return res.status(500).json(err);
+                    }
+                });
+            } else {
+                return res.status(400).json({ message: "Something went wrong, please try again later." })
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+
+    })
 })
 
 module.exports = router;  
